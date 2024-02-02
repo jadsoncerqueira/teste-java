4b2daae494683e9dc61f8f35dd91312861689c70
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2024-02-02 19:27:39 -0300

    Implementando a classe Pessoa com os atributos nome e data de nascimento

---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+    String  _name;
+    LocalDate _dataNascimento;
+    public Pessoa(String name, LocalDate dataNascimento) {
+        _name = name;
+        _dataNascimento = dataNascimento;
+    }
+}
